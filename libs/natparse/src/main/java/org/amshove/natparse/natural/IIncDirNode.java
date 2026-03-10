0afaf5657c828849a99b22e303cc6db542c11501
Adrian Wilkins <adrian.wilkins@madetech.com>
2026-03-10 16:40:27 +0000

    Beginnings of some map parsing
    - Progress : the define data block in map rules is parsed
    - Todo : there is no access to the new variables so the code checkers
    still hates them

--- libs/natparse/src/main/java/org/amshove/natparse/natural/IIncDirNode.java
+++ libs/natparse/src/main/java/org/amshove/natparse/natural/IIncDirNode.java
@@ -1,4 +1,10 @@
 package org.amshove.natparse.natural;
 
+import org.amshove.natparse.lexing.SyntaxToken;
+
 public interface IIncDirNode extends IStatementNode
-{}
+{
+	SyntaxToken ddmName();
+	SyntaxToken fieldName();
+
+}
