154f238f89310a2aeee693a014fbfeda1fa793ee
Adrian Wilkins <adrian.wilkins@madetech.com>
2026-07-30 00:12:10 +0100

    You can concat unicode (but not with alpha)

--- libs/natls/src/test/java/org/amshove/natls/FindConstantsEndpointShould.java
+++ libs/natls/src/test/java/org/amshove/natls/FindConstantsEndpointShould.java
@@ -50,6 +50,8 @@ class FindConstantsEndpointShould extends EmptyProjectTest
 			1 C-N (N1) CONST<2>
 			1 C-L (L) CONST<TRUE>
 			1 C-CONCAT (A6) CONST<'abc' - 'def'>
+			1 U-CONCAT (U6) CONST <U'abc' - U'def'>
+			1 UH-CONCAT (U6) CONST <UH'00410042' - U'CDEF'>
 			END-DEFINE
 			""");
 
@@ -60,12 +62,14 @@ class FindConstantsEndpointShould extends EmptyProjectTest
 			""");
 
 		var response = getContext().server().findConstants(new FindConstantsParams(identifier)).get(1, TimeUnit.MINUTES);
-		assertThat(response.getConstants()).hasSize(4);
+		assertThat(response.getConstants()).hasSize(6);
 
 		assertConstantWithValue(response, "C-A", "'A'");
 		assertConstantWithValue(response, "C-N", "2");
 		assertConstantWithValue(response, "C-L", "TRUE");
 		assertConstantWithValue(response, "C-CONCAT", "'abcdef'");
+		assertConstantWithValue(response, "U-CONCAT", "'abcdef'");
+		assertConstantWithValue(response, "UH-CONCAT", "'ABCDEF'");
 	}
 
 	@Test
